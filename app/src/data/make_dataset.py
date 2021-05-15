a42bedac6f6edbab58ebd02ffc10f7c1b985428a
Manuel Zaera-Sanz <manuelzaerasanz@ess.eu>
2021-05-15 16:52:47 +0200

    adapting files to our problem

--- app/src/data/make_dataset.py
+++ app/src/data/make_dataset.py
@@ -30,11 +30,7 @@ def make_dataset(path, timestamp, target, cols_to_remove, model_type='RandomFore
     train_df, test_df = train_test_split(df, test_size=0.2, random_state=50)
     print('---> Transforming data')
     train_df, test_df = transform_data(train_df, test_df, timestamp, target, cols_to_remove)
-    print('---> Feature engineering')
-    train_df, test_df = feature_engineering(train_df, test_df)
-    print('---> Preparing data for training')
-    train_df, test_df = pre_train_data_prep(train_df, test_df, model_type, timestamp, target)
-
+   
     return train_df.copy(), test_df.copy()
 
 
@@ -76,109 +72,13 @@ def transform_data(train_df, test_df, timestamp, target, cols_to_remove):
     train_df = remove_unwanted_columns(train_df, cols_to_remove)
     test_df = remove_unwanted_columns(test_df, cols_to_remove)
 
-    # Quitando valores nulos en la variable objetivo
-    print('------> Removing missing targets')
-    train_df = remove_missing_targets(train_df, target)
-    test_df = remove_missing_targets(test_df, target)
+    #Establezco como indice la columna 'index'
+    train_df.set_index('index', inplace=True) 
+    test_df.set_index('index', inplace=True)
 
-    # cambio de tipo
-    train_df['Pclass'] = train_df['Pclass'].astype(str)
-    test_df['Pclass'] = test_df['Pclass'].astype(str)
-
-
-    # Separamos la variable objetivo antes del encoding
-    train_target = train_df[target].copy()
-    test_target = test_df[target].copy()
-    train_df.drop(columns=[target], inplace=True)
-    test_df.drop(columns=[target], inplace=True)
-
-    # generación de dummies
-    print('------> Encoding data')
-    train_df = pd.get_dummies(train_df)
-    test_df = pd.get_dummies(test_df)
-    # alineación de train y test para tener las mismas columnas
-    train_df, test_df = train_df.align(test_df, join='inner', axis=1)
-
-    # guardando las columnas resultantes en IBM COS
-    print('---------> Saving encoded columns')
-    cos.save_object_in_cos(train_df.columns, 'encoded_columns', timestamp)
-
-    # volvemos a unir la variable objetivo a los datasets
-    train_df.reset_index(drop=True, inplace=True)
-    test_df.reset_index(drop=True, inplace=True)
-    train_target.reset_index(drop=True, inplace=True)
-    test_target.reset_index(drop=True, inplace=True)
-    train_df = train_df.join(train_target)
-    test_df = test_df.join(test_target)
-
-    return train_df.copy(), test_df.copy()
-
-
-def pre_train_data_prep(train_df, test_df, model_type, timestamp, target):
-    """
-        Función que realiza las últimas transformaciones sobre los datos
-        antes del entrenamiento (imputación de nulos y escalado)
-
-        Args:
-           train_df (DataFrame):  Dataset de train.
-           test_df (DataFrame):  Dataset de test.
-           model_type (str):  Tipo de modelo a usar.
-           timestamp (float):  Representación temporal en segundos
-           target (str):  Variable dependiente a usar.
-
-        Returns:
-           DataFrame, DataFrame. Datasets de train y test para el modelo.
-    """
-
-    # Separamos la variable objetivo antes de la imputación y escalado
-    train_target = train_df[target].copy()
-    test_target = test_df[target].copy()
-    train_df.drop(columns=[target], inplace=True)
-    test_df.drop(columns=[target], inplace=True)
-
-    # imputación de nulos
-    print('------> Inputing missing values')
-    train_df, test_df = input_missing_values(train_df, test_df, timestamp)
-
-    # restringimos el escalado solo a ciertos modelos
-    if model_type.upper() in ['SVM', 'KNN', 'NaiveBayes']:
-        print('------> Scaling features')
-        train_df, test_df = scale_data(train_df, test_df)
-
-    # volvemos a unir la variable objetivo a los datasets
-    train_df.reset_index(drop=True, inplace=True)
-    test_df.reset_index(drop=True, inplace=True)
-    train_target.reset_index(drop=True, inplace=True)
-    test_target.reset_index(drop=True, inplace=True)
-    train_df = train_df.join(train_target)
-    test_df = test_df.join(test_target)
-
-    return train_df.copy(), test_df.copy()
-
-
-def input_missing_values(train_df, test_df, timestamp):
-    """
-        Función para la imputación de nulos
-
-        Args:
-           train_df (DataFrame):  Dataset de train.
-           test_df (DataFrame):  Dataset de test.
-           timestamp (float):  Representación temporal en segundos.
-
-        Returns:
-           DataFrame, DataFrame. Datasets de train y test para el modelo.
-    """
-    # creamos el imputador que usará la mediana como sustitutivo
-    imputer = SimpleImputer(strategy='median')
-
-    # ajustamos las medianas en base a los datos de train
-    train_df = pd.DataFrame(imputer.fit_transform(train_df), columns=train_df.columns)
-    # imputamos los datos de test
-    test_df = pd.DataFrame(imputer.transform(test_df), columns=test_df.columns)
-
-    # guardamos el imputador para futuros nuevos datos
-    print('------> Saving imputer on the cloud')
-    cos.save_object_in_cos(imputer, 'imputer', timestamp)
+    # guardando las columnas en IBM COS
+    print('---------> Saving predictors and target')
+    cos.save_object_in_cos(train_df.columns, 'predictors_and_target', timestamp)
 
     return train_df.copy(), test_df.copy()
 
@@ -194,40 +94,4 @@ def remove_unwanted_columns(df, cols_to_remove):
            DataFrame. Dataset.
     """
     return df.drop(columns=cols_to_remove)
-
-
-def remove_missing_targets(df, target):
-    """
-        Función para quitar los valores nulos en la variable objetivo
-
-        Args:
-           df (DataFrame):  Dataset.
-
-        Returns:
-           DataFrame. Dataset.
-    """
-    return df[~df[target].isna()].copy()
-
-
-def scale_data(train_df, test_df):
-    """
-        Función para el escalado de variables
-
-        Args:
-           train_df (DataFrame):  Dataset de train.
-           test_df (DataFrame):  Dataset de test.
-
-        Returns:
-           DataFrame, DataFrame. Datasets de train y test para el modelo.
-    """
-
-    # objeto de escalado en el rango (0,1)
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    # ajuste y escalado en datos de train
-    train_df = scaler.fit_transform(train_df)
-    # escalado de datos de test
-    test_df = scaler.transform(test_df)
-
-    return train_df.copy(), test_df.copy()
-
 
--- app/src/features/feature_engineering.py
+++ app/src/features/feature_engineering.py
@@ -11,22 +11,7 @@ def feature_engineering(train_df, test_df):
         Returns:
            DataFrame, DataFrame. Datasets de train y test para el modelo.
     """
-    train_df = create_domain_knowledge_features(train_df)
-    test_df = create_domain_knowledge_features(test_df)
+    
+    #Not used in this problem
 
     return train_df.copy(), test_df.copy()
-
-
-def create_domain_knowledge_features(df):
-    """
-        Función la creación de variables de contexto
-
-        Args:
-           df (DataFrame):  Dataset.
-        Returns:
-           DataFrame. Dataset.
-    """
-    # creación de variable Child de tipo booleana
-    df['Child'] = 0
-    df.loc[df.Age < 16, 'Child'] = 1
-    return df.copy()
